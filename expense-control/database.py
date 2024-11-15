beccf6c15700b1b477638e393ff8453e77f6161c
chekist <georg4932@mail.ru>
2024-11-15 18:03:59 +1000

    Create session generator

---
+++ expense-control/database.py
@@ -0,0 +1,13 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+
+from config import get_settings
+
+engine = create_async_engine(get_settings().database_url, echo=False)
+async_session_maker = async_sessionmaker(bind=engine, class_=AsyncSession)
+
+
+async def get_async_session() -> AsyncGenerator:
+    async with async_session_maker() as session:
+        yield session
