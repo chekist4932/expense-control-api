1030187795f697c31fe1bda1b2b01ba6d417e51c
chekist <georg4932@mail.ru>
2024-11-17 17:38:36 +1000

    Add validators to app settings

--- expense_control/config.py
+++ expense_control/config.py
@@ -3,7 +3,7 @@ from functools import lru_cache
 from typing import Optional
 from pathlib import Path
 
-from pydantic import PostgresDsn, field_validator, ValidationInfo, IPvAnyAddress
+from pydantic import PostgresDsn, field_validator, ValidationInfo, IPvAnyAddress, ValidationError
 from pydantic_settings import BaseSettings
 from pydantic_core import MultiHostUrl
 
@@ -18,12 +18,27 @@ class BaseConfig(BaseSettings):
         extra = 'ignore'
 
 
+# noinspection PyNestedDecorators
 class AppSettings(BaseConfig):
     APP_NAME: str
     APP_IP: str
     APP_PORT: int
 
+    @field_validator('APP_IP', mode='before')
+    @classmethod
+    def assemble_ip(cls, field_value: Optional[str], values: ValidationInfo) -> str:
+        if field_value == 'localhost':
+            return field_value
+        return str(IPvAnyAddress(field_value))
 
+    @field_validator('APP_PORT', mode='after')
+    @classmethod
+    def assemble_port(cls, field_value: Optional[int], values: ValidationInfo) -> int:
+        assert field_value < pow(2, 16)  # 65536
+        return field_value
+
+
+# noinspection PyNestedDecorators
 class DatabaseSettings(BaseConfig):
     DB_HOST: str
     DB_PORT: int
@@ -38,6 +53,7 @@ class DatabaseSettings(BaseConfig):
     def assemble_db_url(cls, field_value: Optional[PostgresDsn], values: ValidationInfo) -> PostgresDsn:
         if isinstance(field_value, MultiHostUrl):
             return field_value
+
         return PostgresDsn.build(
             scheme="postgresql+asyncpg",
             username=values.data.get('POSTGRES_USER'),
@@ -57,5 +73,6 @@ def get_db_settings() -> DatabaseSettings:
 def get_app_settings() -> AppSettings:
     return AppSettings()
 
-# print(get_db_settings().DATABASE_URI.unicode_string())
+
+# print(get_db_settings())
 # print(get_app_settings())
