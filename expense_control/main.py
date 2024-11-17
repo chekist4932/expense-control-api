491dd35a6fdd3aa745fdd54e38e86357c0334278
chekist <georg4932@mail.ru>
2024-11-17 17:05:01 +1000

    Add start app from main.py

--- expense_control/main.py
+++ expense_control/main.py
@@ -1,14 +1,19 @@
+from uvicorn import run
+
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from expense_control.exceptions import register_exception_handlers
-from database import session_manager
-from config import get_db_settings, get_app_settings
+from expense_control.database import session_manager
+from expense_control.config import get_db_settings, get_app_settings
+
+from expense_control.expense import expense_router
+from expense_control.category import category_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    session_manager.init(get_db_settings().DATABASE_URI)
+    session_manager.init(get_db_settings().DATABASE_URI.unicode_string())
     yield
     await session_manager.close()
 
@@ -16,3 +21,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title=get_app_settings().APP_NAME, lifespan=lifespan)
 
 register_exception_handlers(app)
+app.include_router(category_router)
+app.include_router(expense_router)
+
+
+if __name__ == '__main__':
+    run(app=app,
+        host=get_app_settings().APP_IP,
+        port=get_app_settings().APP_PORT,
+        reload=True,
+        proxy_headers=True
+        )
