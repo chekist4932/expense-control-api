98a0534611d4fb7653df50c57b06697bec417926
chekist <georg4932@mail.ru>
2024-11-19 20:28:44 +1000

    Add category rate field
    Add rate column to category model.
    Add CHECK for rate.
    Add rate field for CategoryBase schema. Add validator for rate.

--- expense_control/category/model.py
+++ expense_control/category/model.py
@@ -1,11 +1,15 @@
-from sqlalchemy import Column, Integer, UniqueConstraint, String
+from sqlalchemy import Column, Integer, UniqueConstraint, String, CheckConstraint
 
 from expense_control.base import Base
 
 
 class Category(Base):
     __tablename__ = 'category'
-    __table_args__ = (UniqueConstraint('name'),)
+    __table_args__ = (UniqueConstraint('name'),
+                      CheckConstraint("rate > 0 AND rate < 11",
+                                      name="chk_rate"),
+                      )
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, nullable=False)
+    rate = Column(Integer, nullable=False)
--- expense_control/category/schemas.py
+++ expense_control/category/schemas.py
@@ -1,12 +1,23 @@
-from pydantic import BaseModel
+from typing import Optional
+from pydantic import BaseModel, field_validator, ValidationInfo
 
 
 class CategoryBase(BaseModel):
     name: str
+    rate: int = 1
+
+    @field_validator('rate', mode='after')
+    @classmethod
+    def validate_rate(cls, field_value: int, values: ValidationInfo) -> int:
+        if not (1 <= field_value <= 10):  # Проверяем диапазон 1–10
+            raise ValueError("The 'rate' must be between 1 and 10 inclusive")
+
+        return field_value
 
 
 class CategoryUpdate(CategoryBase):
-    ...
+    name: Optional[str] = None
+    rate: Optional[int] = None
 
 
 class CategoryCreate(CategoryBase):
