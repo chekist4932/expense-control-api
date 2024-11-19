aab6bc61d643f07b94d8942a74d29182da134153
chekist <georg4932@mail.ru>
2024-11-19 20:29:16 +1000

    Define update

--- expense_control/base/service.py
+++ expense_control/base/service.py
@@ -22,28 +22,42 @@ class BaseService(Generic[Model, CreateSchema, UpdateSchema]):
         self.model = model
         self.database_session = database_session
 
-    async def get_by_id(self, item_id: int) -> Optional[Model]:
-        obj: Optional[Model] = await self.database_session.get(self.model, item_id)
-        if obj is None:
+    async def get_by_id(self, obj_id: int) -> Optional[Model]:
+        db_obj: Optional[Model] = await self.database_session.get(self.model, obj_id)
+        if db_obj is None:
             raise NoResultFound
-        return obj
+        return db_obj
 
     async def get_all(self) -> Optional[list[Model]]:
         query = select(self.model)
-        objs = await self.database_session.execute(query)
-        if not (objs := objs.scalars().all()):
+        db_objs = await self.database_session.execute(query)
+        if not (db_objs := db_objs.scalars().all()):
             raise NoResultFound
-        return [obj for obj in objs]
+        return [db_obj for db_obj in db_objs]
 
-    async def create(self, item_body: CreateSchema) -> Model:
-        obj = self.model(**item_body.model_dump())
+    async def create(self, obj: CreateSchema) -> Model:
+        db_obj = self.model(**obj.model_dump())
 
-        self.database_session.add(obj)
+        self.database_session.add(db_obj)
         try:
             await self.database_session.commit()
         except IntegrityError:
             await self.database_session.rollback()
             raise
-        await self.database_session.refresh(obj)
+        await self.database_session.refresh(db_obj)
 
-        return obj
+        return db_obj
+
+    async def update(self, obj_id: int, obj: UpdateSchema | CreateSchema) -> Model:
+        db_obj: Model = await self.database_session.get(self.model, obj_id)
+        if db_obj:
+            for key, value in obj.dict(exclude_none=True).items():
+                setattr(db_obj, key, value)
+            try:
+                await self.database_session.commit()
+            except IntegrityError:
+                await self.database_session.rollback()
+                raise
+            await self.database_session.refresh(db_obj)
+            return db_obj
+        raise NoResultFound
