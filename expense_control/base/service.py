3853b03f2932bd511d925175fee53ef1c16fa690
chekist <georg4932@mail.ru>
2024-11-21 20:22:12 +1000

    Refactore BaseService

--- expense_control/base/service.py
+++ expense_control/base/service.py
@@ -21,46 +21,47 @@ class BaseService(Generic[Model, CreateSchema, UpdateSchema]):
 
     async def get_by_id(self, obj_id: int) -> Optional[Model]:
         db_obj: Optional[Model] = await self.database_session.get(self.model, obj_id)
-        if db_obj is None:
+
+        if not db_obj:
             raise NoResultFound
+
         return db_obj
 
     async def get_all(self) -> Optional[list[Model]]:
         query = select(self.model)
         db_objs = await self.database_session.execute(query)
+
         if not (db_objs := db_objs.scalars().all()):
             raise NoResultFound
+
         return [db_obj for db_obj in db_objs]
 
     async def create(self, obj: CreateSchema) -> Model:
         db_obj = self.model(**obj.model_dump())
-
         self.database_session.add(db_obj)
-        try:
-            await self.database_session.commit()
-        except DatabaseError:
-            await self.database_session.rollback()
-            raise
+
+        await self.database_session.commit()
         await self.database_session.refresh(db_obj)
 
         return db_obj
 
     async def update(self, obj_id: int, obj: UpdateSchema | CreateSchema) -> Model:
         db_obj: Model = await self.database_session.get(self.model, obj_id)
-        if db_obj:
-            for key, value in obj.dict(exclude_none=True).items():
-                setattr(db_obj, key, value)
-            try:
-                await self.database_session.commit()
-            except DatabaseError:
-                await self.database_session.rollback()
-                raise
-            await self.database_session.refresh(db_obj)
-            return db_obj
-        raise NoResultFound
+
+        if not db_obj:
+            raise NoResultFound
+
+        for key, value in obj.dict(exclude_none=True).items():
+            setattr(db_obj, key, value)
+
+        await self.database_session.commit()
+        await self.database_session.refresh(db_obj)
+
+        return db_obj
 
     async def delete(self, obj_id: int) -> None:
         db_obj = await self.database_session.get(self.model, obj_id)
+
         if not db_obj:
             raise NoResultFound
 
